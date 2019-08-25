e014bece8599a0fe06c7339e8289723c6b2212b2
ismail1199 <ismail32cse@gmail.com>
2019-08-25 17:06:34 +0600

    lll

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,14 @@
 
 use App\Jobs\ProcessPodcast;
 
-Route::get('/', function () {
-   return view('welcome');
-});
+// Route::get('/', function () {
+//    return dd('hello');
+// });
+
+Route::get('/', 'frontend\PagesController@index')->name('index');
 
 Auth::routes();
+
 
 Route::get('/home', 'HomeController@index')->name('home');
 
